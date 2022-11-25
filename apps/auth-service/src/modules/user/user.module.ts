3e1005c486ad575ce9bddcd20bec122a12ef3041
Afsal K <makafsal@gmail.com>
2022-11-25 23:52:07 +0530

    typo

--- apps/auth-service/src/modules/user/user.module.ts
+++ apps/auth-service/src/modules/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CassandraService } from '../cassandra/cassandra.service';
 import { UserController } from './user.controller';
-import { UserRepository } from './user.respository';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
